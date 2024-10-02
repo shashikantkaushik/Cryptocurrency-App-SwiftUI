81dea3d3484ce592a8410d6a9117b28014c71713
Shashi Kant <shashikantkaushik4@gmail.com>
2024-10-02 13:44:42 +0530

    Initial Commit

---
+++ Cryptocurrency SwiftUI/Cryptocurrency_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cryptocurrency_SwiftUIApp.swift
+//  Cryptocurrency SwiftUI
+//
+//  Created by Shashi Kant on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Cryptocurrency_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
